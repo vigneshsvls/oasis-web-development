1ac08760bc5e97294c9b5efd3556a2a22211efae
vigneshsvls <136967227+vigneshsvls@users.noreply.github.com>
2024-07-14 19:14:07 +0530

    Add files via upload

---
+++ task-2/spt.js
@@ -0,0 +1,7 @@
+var typingEffect = new Typed(".typedText", {
+    strings: ["Developer", "Designer", "Coder","Problem solver"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 500
+});
